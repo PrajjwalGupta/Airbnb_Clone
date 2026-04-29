77265ac6889880da8de25982efbe0cf9f38f1202
Prajjwal Gupta <31537066+PrajjwalGupta@users.noreply.github.com>
2026-04-29 23:05:25 +0530

    Initial Commit

---
+++ Airbnb_Clone/Airbnb_CloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Airbnb_CloneApp.swift
+//  Airbnb_Clone
+//
+//  Created by Prajjwal on 29/04/26.
+//
+
+import SwiftUI
+
+@main
+struct Airbnb_CloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
