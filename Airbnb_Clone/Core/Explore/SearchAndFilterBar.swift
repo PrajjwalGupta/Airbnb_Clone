e80f7b59178efe45f548b27643e12c81055badb8
Prajjwal Gupta <31537066+PrajjwalGupta@users.noreply.github.com>
2026-05-02 17:30:38 +0530

    listing detail view

---
+++ Airbnb_Clone/Core/Explore/SearchAndFilterBar.swift
@@ -0,0 +1,18 @@
+//
+//  SearchAndFilterBar.swift
+//  Airbnb_Clone
+//
+//  Created by Prajjwal on 01/05/26.
+//
+
+import SwiftUI
+
+struct SearchAndFilterBar: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SearchAndFilterBar()
+}
